b5aa2c32e5ceeb545fb6d7ce7fe401c7eed24552
Keterr <kartelant@gmail.com>
2018-04-09 05:09:30 -0700

    add console

---
+++ Assets/Scripts/Console.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Console : MonoBehaviour {
+
+    public InputField output;
+    public InputField input;
+    public int linesToDisplay;
+
+    private void Start()
+    {
+        input.caretWidth = 10;
+    }
+
+    public void SubmitText(string text)
+    {
+        string[] args = text.Split(' ');
+        output.text += "\n> " + text + "\n ";
+        if (Command.list.ContainsKey(args[0]))
+            output.text += Command.list[args[0]].Execute(args);
+        else
+            output.text += "Unknown command";
+        input.text = "";
+        input.Select();
+        input.ActivateInputField();
+        FitText();
+    }
+
+    void FitText()
+    {
+        string[] lines = output.text.Split('\n');
+        output.text = "";
+        for(int i = Mathf.Max(lines.Length-linesToDisplay,0); i<lines.Length; i++)
+        {
+            if(i != lines.Length - 1)
+                output.text += lines[i] + "\n";
+            else
+                output.text += lines[i];
+        }
+
+    }
+
+    abstract class Command
+    {
+        public static Dictionary<string, Command> list = new Dictionary<string, Command>()
+        {
+            {"help",new HelpCommand() },
+            {"echo",new EchoCommand() }
+        };
+        abstract public string Execute(string[] args);
+
+        class HelpCommand : Command
+        {
+            public override string Execute(string[] args)
+            {
+                return "Not implemented";
+            }
+        }
+
+        class EchoCommand : Command
+        {
+            public override string Execute(string[] args)
+            {
+                string output = "";
+                for(int i = 1; i < args.Length; i++)
+                    output += args[i] + " ";
+                return output;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject console;
+    public Transform consoleOff;
+    public Transform consoleOn;
+
+    bool transitioning;
+    Transform transitionTransform;
+    Vector3 transitionTarget;
+    Vector3 transitionOrigin;
+    float transitionRate;
+
+    bool consoleEnabled = false;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<PlayerMovement>().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transitioning && (transitionTransform.position = Vector3.Lerp(transitionOrigin, transitionTarget, ((transitionTransform.position - transitionOrigin).magnitude / (transitionTarget - transitionOrigin).magnitude) + Time.deltaTime * transitionRate)) == transitionTarget)
+        {
+            console.GetComponentInChildren<InputField>().interactable = consoleEnabled;
+            if (consoleEnabled) console.GetComponentInChildren<InputField>().Select();
+            transitioning = false;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                console.GetComponentInChildren<InputField>().text = "";
+                consoleEnabled = !consoleEnabled;
+                BeginTransition(console.transform, consoleEnabled ? consoleOn.position : consoleOff.position, 3);
+                GetComponent<PlayerMovement>().enabled = !consoleEnabled;
+            }
+        }
+	}
+
+    public void BeginTransition(Transform tr, Vector3 targ, float rate)
+    {
+        transitioning = true;
+        transitionTransform = tr;
+        transitionOrigin = tr.position;
+        transitionTarget = targ;
+        transitionRate = rate;
+    }
+}
