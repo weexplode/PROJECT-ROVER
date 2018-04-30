469c4caa5ec266ae4bf2dc5f3cedd81b0d931a1b
Keterr <kartelant@gmail.com>
2018-04-29 20:36:44 -0700

    removing old assets, working door

---
+++ Assets/Scripts/ProximityDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityDoor : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Animator>().SetBool("character_nearby", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<Animator>().SetBool("character_nearby", false);
+    }
+}
